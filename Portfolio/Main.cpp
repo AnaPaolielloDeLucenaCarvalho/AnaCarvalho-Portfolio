883576056780c3caa2d1adaa2388a40ca3d66a41
Ana Carvalho <ana.paoliello.de.lucena.carvalho@student.howest.be>
2026-04-26 21:00:04 +0200

    added the background (this will change)
    - Added the final image result from the photoshop, this will of course change to singular sprites so the player can have interaction with the map but for now it works to see where each sprite needs to go

--- Portfolio/Main.cpp
+++ Portfolio/Main.cpp
@@ -3,9 +3,23 @@
 #include "Minigin.h"
 #include <iostream>
 
+#include "Scene.h"
+#include "SceneManager.h"
+#include "GameObject.h"
+#include "RenderComponent.h"
+#include "ResourceManager.h"
+#include <utility>
+
 void load()
 {
     std::cout << "Welcome to the Portfolio!\n";
+
+    auto& scene = dae::SceneManager::GetInstance().CreateScene();
+
+    auto background = std::make_unique<dae::GameObject>();
+    background->AddComponent<dae::RenderComponent>("MainMenu Background.png");
+    background->SetLocalPosition(0, 0);
+    scene.Add(std::move(background));
 }
 
 int main(int, char* [])
