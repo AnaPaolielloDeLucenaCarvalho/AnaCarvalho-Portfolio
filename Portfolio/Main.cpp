ec02bac2415eb9c12431d01fec296b1115428966
Ana Carvalho <ana.paoliello.de.lucena.carvalho@student.howest.be>
2026-04-26 21:12:02 +0200

    name correction and fixing data folder for web
    Revert "test"
    
    This reverts commit 38f4510970f9da1acbaae4782c5a1db3bd7f91ce.
    
    test

--- Portfolio/Main.cpp
+++ Portfolio/Main.cpp
@@ -9,6 +9,9 @@
 #include "RenderComponent.h"
 #include "ResourceManager.h"
 #include <utility>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 void load()
 {
@@ -17,7 +20,7 @@ void load()
     auto& scene = dae::SceneManager::GetInstance().CreateScene();
 
     auto background = std::make_unique<dae::GameObject>();
-    background->AddComponent<dae::RenderComponent>("MainMenu Background.png");
+    background->AddComponent<dae::RenderComponent>("MainMenuBackground.png");
     background->SetLocalPosition(0, 0);
     scene.Add(std::move(background));
 }
@@ -26,8 +29,17 @@ int main(int, char* [])
 {
     std::cout << "Starting Portfolio Engine...\n";
 
-    // Start the engine
-    dae::Minigin engine("./Data/");
+#ifdef __EMSCRIPTEN__
+    fs::path data_location = "";
+#else
+    fs::path data_location = "./Data/";
+    if (!fs::exists(data_location))
+    {
+        data_location = "../Data/";
+    }
+#endif
+
+    dae::Minigin engine(data_location);
     engine.Run(load);
 
     return 0;
