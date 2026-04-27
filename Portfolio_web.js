42a2b57e9db0695818cba22af2f5d13d63750426
AnaPaolielloDeLucenaCarvalho <AnaPaolielloDeLucenaCarvalho@users.noreply.github.com>
2026-04-27 07:51:20 +0000

    deploy: 8fb19b13ef41803f84e90b2eb2131189c39f3e79

--- Portfolio_web.js
+++ Portfolio_web.js
@@ -29,7 +29,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpsl_9bpjv.js
+// include: /tmp/tmp5ld68x_7.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -161,7 +161,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
   })();
 
-// end include: /tmp/tmpsl_9bpjv.js
+// end include: /tmp/tmp5ld68x_7.js
 
 
 var arguments_ = [];
